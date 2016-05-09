2bd9c577b137c339e8fcf290a10b5259f83e7c38
joe.nuspl <joe.nuspl@workday.com>
2016-05-09 14:21:32 -0700

    v0.11.3
    Change-Id: I10aff72ade505d158e2f1c5bc739e50f9adf7629

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ maintainer_email  'nuspl@nvwls.com'
 license           'Apache 2.0'
 description       'Provides HWRPs for creating authoritative resources'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '0.11.2'
+version           '0.11.3'
